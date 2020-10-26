1b7fd07f75bb1815739fa26a724a7245677c4fe2
Александр Mazharov <a.mazharov@gmail.com>
2020-10-26 11:42:24 +0400

    add Task8

---
+++ Task8OOP/entity/Genre.js
@@ -0,0 +1,27 @@
+ export class Genre{
+
+    _name
+    _description
+
+    constructor(name, description) {
+        this._name = name;
+        this._description = description;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(value) {
+        this._description = value;
+    }
+}
+
