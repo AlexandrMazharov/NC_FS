71ff9822a301852cf304a41637ff5375f45b5ba7
Александр Mazharov <a.mazharov@gmail.com>
2020-11-05 15:12:23 +0400

    add fix

--- task5.js
+++ task5.js
@@ -13,50 +13,79 @@ class CustomForm {
     this.mess = mess;
   }
 }
+window.onload = function () {
+  // let f = document.forms[0];
+  console.log("1");
+  const old = getOldForm();
+  completeForm(old);
+  formChange();
+};
+function getOldForm() {
+  let currentform = new CustomForm("", "", "", "", "");
 
-let f = document.forms[0];
-let currentform = new CustomForm("", "", "", "", "");
-let fnLabel = document.getElementsByClassName("fistName-label");
-let lnLabel = document.getElementsByClassName("lastName-label");
-let telLabel = document.getElementsByClassName("tel-label");
-let emailLabel = document.getElementsByClassName("email-label");
-let messLabel = document.getElementsByClassName("mes-label");
-console.log(messLabel);
-let fn = f.elements[0];
-let ln = f.elements[1];
-let tel = f.elements[2];
-let email = f.elements[3];
-let mes = f.elements[4];
+  if (localStorage.getItem("oldform")) {
+    let oldform = JSON.parse(localStorage.getItem("oldform"));
+    console.log("getOldFormOk");
+    currentform.firstName = oldform["firstName"];
+    currentform.lastName = oldform["lastName"];
+    currentform.tel = oldform["tel"];
+    currentform.email = oldform["email"];
+    currentform.mess = oldform["mess"];
+
+    return currentform;
+  }
+  return;
+}
 
 function formChange() {
+  let f = document.forms[0];
+  let fnLabel = document.getElementsByClassName("fistName-label");
+  let lnLabel = document.getElementsByClassName("lastName-label");
+  let telLabel = document.getElementsByClassName("tel-label");
+  let emailLabel = document.getElementsByClassName("email-label");
+  let messLabel = document.getElementsByClassName("mes-label");
+  console.log(messLabel);
+  let fn = f.elements[0];
+  let ln = f.elements[1];
+  let tel = f.elements[2];
+  let email = f.elements[3];
+  let mes = f.elements[4];
+
   let btn = document.getElementById("formsubmit");
   btn.disabled = false;
-  if (!checkFN(fn, fnLabel)) btn.disabled = true;
-  if (!checkLn(ln, lnLabel)) btn.disabled = true;
-  if (!checkEmail(email, emailLabel)) btn.disabled = true;
-  if (!checkTel(tel, telLabel)) btn.disabled = true;
-  if (!checkMess(mes, messLabel)) btn.disabled = true;
-}
-// если есть сохраненная старая форма
-if (localStorage.getItem("oldform")) {
-  let oldform = JSON.parse(localStorage.getItem("oldform"));
+  let fnIsValid = false;
+  let lnIsValid = false;
+  let tellIsValid = false;
+  let emailIsValid = false;
+  let messIsValid = false;
 
-  currentform.firstName = oldform["firstName"];
-  currentform.lastName = oldform["lastName"];
-  currentform.tel = oldform["tel"];
-  currentform.email = oldform["email"];
-  currentform.mess = oldform["mess"];
-  completeForm();
+  if (fn.value != "") if (checkFN(fn, fnLabel)) fnIsValid = true;
+  if (ln.value != "") if (checkLn(ln, lnLabel)) lnIsValid = true;
+  if (email.value != "") {
+    if (checkEmail(email, emailLabel)) emailIsValid = true;
+  }
+  if (checkTel(tel, telLabel)) tellIsValid = true;
+  if (mes.value != "") {
+    if (checkMess(mes, messLabel)) messIsValid = true;
+  }
+  if (fnIsValid && lnIsValid && tellIsValid && emailIsValid && messIsValid) {
+    btn.disabled = false;
+  } else {
+    btn.disabled = true;
+  }
 }
+
 // заполняем форму известными данными
-function completeForm() {
+function completeForm(currentform) {
+  let f = document.forms[0];
+
   f.elements[0].value = currentform.firstName;
   f.elements[1].value = currentform.lastName;
   f.elements[2].value = currentform.tel;
   f.elements[3].value = currentform.email;
   f.elements[4].value = currentform.mess;
 }
-formChange();
+
 // сохраняем в local storage
 function saveToLocal(fn, ln, tel, email, mess) {
   let f = new CustomForm(
@@ -99,8 +128,9 @@ function checkLn(ln, lnLabel) {
 }
 function checkTel(tel, telLabel) {
   if (
-    !/^\s*$/.test(tel.value) &&
-    /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(tel.value)
+    (!/^\s*$/.test(tel.value) &&
+      /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(tel.value)) ||
+    tel.value == ""
   ) {
     tel.classList.remove("invalid");
     tel.classList.add("valid");
@@ -155,24 +185,27 @@ function get_cookie(cookie_name) {
 // обрботчик кнопки
 function formClick() {
   let isSent = get_cookie("inSent");
+  let currentform = getOldForm();
   if (isSent) {
     createCustomAlert(
-      `${currentform["firstName"]} ${currentform["lastName"]}, ваше обращение обрабатывается!!`
+      `${currentform["firstName"]} ${currentform["lastName"]}, ваше обращение обрабатывается!!`,
+      "MESSAGE",
+      "Ok"
     );
     // alert(
     //   `<${currentform["firstName"]} ${currentform["lastName"]}>, ваше обращение обрабатывается!!`
     // );
   } else {
     //let userMessageFormValid = ""; // строка с сообщением о невалидных полях
-    console.log(mes);
+
     let formValid = true;
 
-    formValid =
-      checkFN(fn, fnLabel) &&
-      checkLn(ln, lnLabel) &&
-      checkEmail(email, emailLabel) &&
-      checkTel(tel, telLabel) &&
-      checkMess(mes, messLabel);
+    // formValid =
+    //   checkFN(fn, fnLabel) &&
+    //   checkLn(ln, lnLabel) &&
+    //   checkEmail(email, emailLabel) &&
+    //   checkTel(tel, telLabel) &&
+    //   checkMess(mes, messLabel);
 
     console.log(formValid); // true если форма валидна
     if (!formValid) {
@@ -182,8 +215,19 @@ function formClick() {
       //   `Поля ${userMessageFormValid} заполнены не верно, пожалуйста исправьте`
       // );
     } else {
+      let f = document.forms[0];
+      let fn = f.elements[0];
+      let ln = f.elements[1];
+      let tel = f.elements[2];
+      let email = f.elements[3];
+      let mes = f.elements[4];
+
       saveToLocal(fn, ln, tel, email, mes);
-      createCustomAlert(`${fn.value} ${ln.value}, спасибо за обращение!`);
+      createCustomAlert(
+        `${fn.value} ${ln.value}, спасибо за обращение!`,
+        "MESSAGE",
+        "Ok"
+      );
       // alert(`${fn.value} ${ln.value}, спасибо за обращение!`);
       document.cookie = "inSent=true";
     }
@@ -191,11 +235,7 @@ function formClick() {
 }
 
 // CUSTOM ALERT
-
-const ALERT_TITLE = "MESSAGE!";
-const ALERT_BUTTON_TEXT = "Ok";
-
-function createCustomAlert(txt) {
+function createCustomAlert(txt, title, btn_text) {
   d = document;
 
   mObj = d.getElementsByTagName("body")[0].appendChild(d.createElement("div"));
@@ -211,14 +251,14 @@ function createCustomAlert(txt) {
   alertObj.style.visiblity = "visible";
 
   h1 = alertObj.appendChild(d.createElement("h1"));
-  h1.appendChild(d.createTextNode(ALERT_TITLE));
+  h1.appendChild(d.createTextNode(title));
 
   msg = alertObj.appendChild(d.createElement("p"));
   msg.innerHTML = txt;
 
   btn = alertObj.appendChild(d.createElement("a"));
   btn.id = "closeBtn";
-  btn.appendChild(d.createTextNode(ALERT_BUTTON_TEXT));
+  btn.appendChild(d.createTextNode(btn_text));
   btn.href = "#";
   btn.focus();
   btn.onclick = function () {
