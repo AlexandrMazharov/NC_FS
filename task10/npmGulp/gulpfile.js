51b557645202f394a9744764c114ce8045af7bb5
Александр Mazharov <a.mazharov@gmail.com>
2020-11-15 20:58:48 +0400

    add uglify-es

--- task10/npmGulp/gulpfile.js
+++ task10/npmGulp/gulpfile.js
@@ -1,8 +1,11 @@
-const gulp     = require("gulp"),
-    concat = require('gulp-concat');
-
+const gulp = require("gulp"),
+    concat = require('gulp-concat'),
+    // uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify-es').default;
 gulp.task('concat-task', () => {
     return gulp.src('./src/*/*.js')
+
         .pipe(concat('main.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('./dist/'));
 });
